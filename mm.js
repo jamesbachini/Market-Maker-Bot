3330cfbe6419671dfa70023573e02aecf2598c06
TmoCdoot <92098125+TmoCdoot@users.noreply.github.com>
2023-05-29 15:54:35 +0200

    29/05

--- mm.js
+++ mm.js
@@ -1,30 +1,70 @@
 const ethers = require('ethers');
+const prompt = require("prompt-sync")({ sigint: true }) // get user input
+const BigNumber = require('bignumber.js');
 require("dotenv").config();
 
-const wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'; // goerli weth
-//const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // mainnet weth
 const routerAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564'; // Uniswap Router
 const quoterAddress = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'; // Uniswap Quoter
-const tokenAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'; // goerli uni
-const fee = 3000; // Uniswap pool fee bps 500, 3000, 10000
-const buyAmount = ethers.parseUnits('0.001', 'ether');
-const targetPrice = BigInt(35); // target exchange rate
-const targetAmountOut = buyAmount * targetPrice;
-const sellAmount = buyAmount / targetPrice;
-const tradeFrequency = 3600 * 1000; // ms (once per hour)
+var wethAddress = ''
+var provider = ''
+var ethQuantity = ''
+var countBuyToken = ''
+var sellAmount = '';
+var buyAmount = ''
 
-// `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
-const provider = new ethers.JsonRpcProvider(`https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
+var typeChain = prompt("Ethereum (e) or Goerli (g) :  ");
+var tokenAddress = prompt("Token adress : ");
+if (typeChain == 'e') {
+  wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // mainnet eth
+  provider = new ethers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);//eth rpc
+} else if (typeChain == 'g') {
+  wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'; // goerli weth
+  provider = new ethers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);//goerli rpc
+}
+const fee = 3000; // Uniswap pool fee bps 500, 3000, 10000
+var typeTrade = prompt("Selle (s) or Buy (b) :  ");
+if (typeTrade == 's') {
+  sellAmount = prompt("Token quantity :  ");
+  buyAmount = 0
+} else if (typeTrade == 'b') {
+  ethQuantity = prompt("Eth quantity :  ");
+  buyAmount = ethers.parseUnits(ethQuantity, 'ether');// montant d'eth a swap
+}
+
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 const account = wallet.connect(provider);
+
+
+
+
+//const targetPrice = BigInt(35); // target exchange rate
+//const targetAmountOut = buyAmount * targetPrice;
+//const tradeFrequency = 3600 * 1000; // ms (once per hour)
+
+
+//const wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'; // goerli weth
+//const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // mainnet eth
+//const tokenAddress = '0xF5081b106AbbE443FC62384b3724620Fc9599c46'; // goerli uni
+
+
+
+
+
+
+
+
+
+// `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
+//const provider = new ethers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
+
+
 
 const token = new ethers.Contract(
   tokenAddress,
   [
     'function approve(address spender, uint256 amount) external returns (bool)',
     'function allowance(address owner, address spender) public view returns (uint256)',
-  ],
-  account
+  ],account
 );
 
 const router = new ethers.Contract(
@@ -44,6 +84,12 @@ const buyTokens = async () => {
   const deadline = Math.floor(Date.now() / 1000) + 600;
   const tx = await router.exactInputSingle([wethAddress, tokenAddress, fee, wallet.address, deadline, buyAmount, 0, 0], {value: buyAmount});
   await tx.wait();
+  const amountOut = await quoter.quoteExactInputSingle(wethAddress, tokenAddress, fee, buyAmount, 0);
+
+  console.log(`Current Exchange Rate: ${amountOut.toString()}`);
+  //console.log(`Current Exchange Rate: ${((BigNumber(amountOut) * BigNumber(0.000000000000000001)).toPrecision(6)).toString()}`);
+  countBuyToken = (BigNumber(amountOut) * BigNumber(0.000000000000000001)).toPrecision(6)
+  console.log('Quantity buy : ' + countBuyToken)
   console.log(tx.hash);
 }
 
@@ -62,7 +108,28 @@ const sellTokens = async () => {
   console.log(tx.hash);
 }
 
-const checkPrice = async () => {
+if (typeTrade == 's') {
+  sellTokens()
+} else if (typeTrade == 'b') {
+  buyTokens()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//buyTokens()
+//sellTokens()
+
+/*const checkPrice = async () => {
   const amountOut = await quoter.quoteExactInputSingle(wethAddress, tokenAddress, fee, buyAmount, 0);
   console.log(`Current Exchange Rate: ${amountOut.toString()}`);
   console.log(`Target Exchange Rate: ${targetAmountOut.toString()}`);
@@ -73,4 +140,4 @@ const checkPrice = async () => {
 checkPrice();
 setInterval(() => {
   checkPrice();
-}, tradeFrequency);
+}, tradeFrequency)*/
